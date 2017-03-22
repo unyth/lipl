ee3528b828b5d3918dff6fccb7573b2ab8a4daef
yth1 <yuancao85@gmail.com>
2017-03-22 16:49:28 +0000

    Finished reading expressions

--- s_expression.c
+++ s_expression.c
@@ -55,18 +55,57 @@ lval* lval_sexpr() {
 	return v;
 }
 
-lval lval_num(long x) {
-	lval v;
-	v.type = LVAL_NUM;
-	v.num = x;
+//lval destructor
+void lval_del(lval* v) {
+
+	switch(v->type) {
+		case LVAL_NUM: break;
+		case LVAL_ERR: free(v->err); break;
+		case LVAL_SYM: free(v->sym); break;
+		case LVAL_SEXPR: 
+			for (int i = 0; i < v->count; i++)
+				lval_del(v->cell[i]);
+			free(v->cell);
+			break;
+	}
+
+	free(v);
+}
+
+// read mpc_ast_t with number contents into lval
+lval* lval_read_num(mpc_ast_t* t) {
+	errno = 0;
+	long x = strtol(t->contents, NULL, 10);
+	return errno != ERANGE ? lval_num(x) : lval_err("Invalid Number");
+}
+
+// general read mpc_ast_t into lval
+
+lval* lval_add(lval* v, lval* x) {
+	v->count++;
+	v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+	v->cell[v->count-1] = x;
 	return v;
 }
 
-lval lval_err(int x) {
-	lval v;
-	v.type = LVAL_ERR;
-	v.err = x;
-	return v;
+lval* lval_read(mpc_ast_t* t) {
+
+	if(strstr(t->tag, "number")) return lval_read_num(t);
+	if(strstr(t->tag, "symbol")) return lval_sym(t->contents);
+
+	// in case of root (<) or sexpr, create empty list
+	if(strcmp(t->tag, ">" == 0) || strstr(t->tag, "sexpr"))
+		x = lval_sexpr();
+
+	for (int i = 0; i < t->children_num; i++) {
+		if (strcmp(t->children[i]->contents, "(") == 0) continue;
+		if (strcmp(t->children[i]->contents, ")") == 0) continue;
+		if (strcmp(t->children[i]->contents, "}") == 0) continue;
+		if (strcmp(t->children[i]->contents, "{") == 0) continue;
+		if (strcmp(t->children[i]->tag, "regex") == 0) continue;
+		
+		x = lval_add(x, lval_read(t->children[i]));
+	}
 }
 
 void lval_print(lval v) {
