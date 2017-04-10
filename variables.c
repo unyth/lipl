56bae0231100d548aa82b9bc6807952daeb65b5c
yth1 <yuancao85@gmail.com>
2017-04-10 01:47:52 +0000

    Replaced specific symbols with names

--- variables.c
+++ variables.c
@@ -328,8 +328,7 @@ int main (int argc, char **argv) {
 	/* MPC Grammar */
 	mpca_lang(MPCA_LANG_DEFAULT,
 	  " number	: /-?[0-9]+/ ;					\
-	    symbol	: \"list\" | \"head\" | \"tail\" | \"join\" 	\
-	    		| \"eval\" | '+' | '-' | '*' | '/' ;		\
+	    symbol	: /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]*/ ;	 	\
 	    sexpr	: '(' <expr>* ')' ;				\
 	    qexpr	: '{' <expr>* '}' ;				\
 	    expr	: <number> | <symbol> | <sexpr> | <qexpr> ;	\
