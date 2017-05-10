fc73c439aef75deb6dcc4a3aef9d1ae30a474398
yth1 <yuancao85@gmail.com>
2017-05-10 16:59:03 +0000

    Corrected Evaluation

--- variables.c
+++ variables.c
@@ -183,44 +183,6 @@ lval* lval_copy(lval* v) {
 	return x;
 }
 
-lval* lval_eval(lenv* e, lval* v) {
-	if(v->type == LVAL_SYM) {
-		lval* x = lenv_get(e, v);
-		lval_del(v);
-		return x;
-	}	
-
-	if(v->type == LVAL_SEXPR) return lval_eval_sexpr(e, v);
-
-	return v;
-}
-
-lval* lval_eval_sexpr(lenv* e, lval* v) {
-
-	for(int i = 0; i < v->count; i++) {
-		v->cell[i] = lval_eval(e, v->cell[i]);
-	}
-
-	for(int i = 0; i < v->count; i++) {
-		if(v->cell[i]->type == LVAL_ERR) return lval_take(v, i);
-	}
-
-	if (v->count == 0) return v;
-	if (v->count == 1) return lval_take(v, 0);
-
-	lval* f = lval_pop(v, 0);
-
-	if (f->type != LVAL_SYM) {
-		lval_del(f);
-		lval_del(v);
-		return lval_err("S-Expression does not start with a symbol!");
-	}
-
-	lval* result = f->fun(e, v);
-	lval_del(f);
-	return result;
-}
-
 lval* lval_add(lval* v, lval* x) {
 	v->count++;
 	v->cell = realloc(v->cell, sizeof(lval*) * v->count);
@@ -475,6 +437,50 @@ void lenv_add_builtins(lenv* e) {
 }
 
 /* Checked */
+
+/* Evaluation */
+
+lval* lval_eval_sexpr(lenv* e, lval* v) {
+
+	for(int i = 0; i < v->count; i++) {
+		v->cell[i] = lval_eval(e, v->cell[i]);
+	}
+
+	for(int i = 0; i < v->count; i++) {
+		if(v->cell[i]->type == LVAL_ERR) return lval_take(v, i);
+	}
+
+	if (v->count == 0) return v;
+	if (v->count == 1) return lval_take(v, 0);
+
+	lval* f = lval_pop(v, 0);
+
+	if (f->type != LVAL_FUN) {
+		lval* err = lval_err(
+			"S-Expression starts with incorrect type. "
+			"Got %s, expected %s",
+			ltype_name(f->type), ltype_name(LVAL_FUN));
+		lval_del(f);
+		lval_del(v);
+		return err;
+	}
+
+	lval* result = f->fun(e, v);
+	lval_del(f);
+	return result;
+}
+
+lval* lval_eval(lenv* e, lval* v) {
+	if(v->type == LVAL_SYM) {
+		lval* x = lenv_get(e, v);
+		lval_del(v);
+		return x;
+	}	
+
+	if(v->type == LVAL_SEXPR) return lval_eval_sexpr(e, v);
+	
+	return v;
+}
 
 /* Reading */
 
