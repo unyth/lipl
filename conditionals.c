191d9f50f1a7aec92e7f93334bd62be6d9b10d5a
yth1 <yuancao85@gmail.com>
2017-07-17 16:21:28 +0000

    Added partial ordering evaluation

--- conditionals.c
+++ conditionals.c
@@ -354,6 +354,27 @@ void lenv_def(lenv* e, lval* k, lval* v) {
 
 /* Builtins */
 
+lval* builtin_ord(lenv* e, lval* a, char* op) {
+	LASSERT_NUM(op, a, 2);
+	LASSERT_TYPE(op, a, 0, LVAL_NUM);
+	LASSERT_TYPE(op, a, 1, LVAL_NUM);
+
+	int r;
+
+	if (strcmp(op, ">") == 0) r = (a->cell[0]->num > a->cell[1]->num);
+	if (strcmp(op, "<") == 0) r = (a->cell[0]->num < a->cell[1]->num);
+	if (strcmp(op, "<=" == 0) r = (a->cell[0]->num <= a->cell[1]->num);
+	if (strcmp(op, ">=") == 0) r = (a->cell[0]->num >= a->cell[1]->num);
+
+	lval_del(a);
+	return lval_num(r);
+}
+
+lval* builtin_gt(lenv* e, lval* a) { return builtin_ord(e, a, ">"); }
+lval* builtin_lt(lenv* e, lval* a) { return builtin_ord(e, a, "<"); }
+lval* builtin_ge(lenv* e, lval* a) { return builtin_ord(e, a, ">="); }
+lval* builtin_le(lenv* e, lval* a) { return builtin_ord(e, a, ">="); }
+
 lval* builtin_lambda(lenv* e, lval* a) {
 	LASSERT_NUM("\\", a, 2);
 	LASSERT_TYPE("\\", a, 0, LVAL_QEXPR);
